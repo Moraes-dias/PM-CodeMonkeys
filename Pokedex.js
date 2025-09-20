3060a9195ba5a620b1feeb6b01de3c7af3316495
FelipeYuji <fjapa977@gmail.com>
2025-09-20 20:17:16 -0300

    As funções pedidas foram adicionadas

--- Pokedex.js
+++ Pokedex.js
@@ -38,3 +38,20 @@ class status extends pokemon{
 };
 
 */
+
+descricao() //metodo para mostrar as caracteristicas do pokemon
+    { //linhas 15 ate 18
+        return `#${this.numDex} ${this.nome} ${this.regiao} Tipagem: ${this.tipos.join(", ")} ${this.favorito}`
+    }            
+
+    mostrarValores() //mostrar somente os status
+//linhas 33 ate 36
+    {
+        return `hp: ${this.hp} atk fisico: ${this.atkFisico} defesa fisica: ${this.defFisica} ataque especial: ${this.atkEspecial} defesa especial: ${this.defEspecial} velocidade: ${this.velocidade}`
+    }
+
+
+    mostrarDadosUsuario() //linhas 45 ate 48
+    {
+        return `usuario: ${this.usuario} email: ${this.email}`
+    }
