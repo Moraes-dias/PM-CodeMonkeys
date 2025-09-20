6fadc5ba1ef331d2e7c9e35e3a746b485a0abaa0
Gregory <necrondragonss@gmail.com>
2025-09-20 20:06:55 -0300

    Primeira classe construtora com todos os this organizados

--- RodrigoPM.js
+++ RodrigoPM.js
@@ -1,3 +1,19 @@
+class pokemon
+{
+    constructor(numDex = 0, nome = 'padrao', regiao = 'padrao', tipos = [], favorito = false)
+    /*
+        construtor do pokemon necessario fazer 2 classes uma para o 
+        o pokemon em si e uma para os status para assim evitar linhas desnecessarias
+    */
+    {
+        this.numDex = numDex;
+        this.nome = nome;
+        this.regiao = regiao;
+        this.tipos = tipos;
+        this.favorito = favorito;
+    }
+}   
+
 class status extends pokemon{ 
     constructor(Numdex = 0 , psy=0 , def=0 , speed=0 , hp=0){
         this.Numdex = Numdex
