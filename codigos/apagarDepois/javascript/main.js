8d96c98e343048415c0cf19225d7de35ec099b13
pedro <pedro.m.dias.d.moraes@gmail.com>
2025-11-27 20:13:49 -0300

    fiz uma parte da modularizacao

--- codigos/apagarDepois/javascript/main.js
+++ codigos/apagarDepois/javascript/main.js
@@ -1,12 +1,14 @@
 //import * as buscador from 'codigos\javascript\busca.js'
 //import {funcoes} from './criar.js'
 
-//vai chamar provavelmente somente global e controlador
+
 
 import { iniciarGlobal } from "./global";
-
+import { buscar } from "./busca";
 
 document.addEventListener('DOMContentLoaded', ()=>{
 
     iniciarGlobal();
+
+    buscar();
 })
